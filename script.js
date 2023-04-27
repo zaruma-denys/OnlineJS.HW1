d6771be0cabcbcbc9acc1d33c0636f2780eade2d
Zaruma Denys <121362477+zaruma-denys@users.noreply.github.com>
2023-04-27 21:30:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let city = 'Kyiv';
+city = 'Lviv';
+
+let adress;
+adress = city;
+alert(adress);
+
+let n = 10;
+let result = Math.pow(n,3);
+console.log(result);
+
+let pen = 4;
+let penprice = 5.56;
+let pencil = 6;
+let pencilprice = 3.50;
+
+let resultprice = pen * penprice + pencil * pencilprice;
+document.write(resultprice)
